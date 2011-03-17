8fd6d907b0c2c8db09abf1dc661880ad562ee428
Aaron Sheriff <aaron.sheriff@gmail.com>
2011-03-17 10:47:45 -0500

    Dryed pages spec

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,25 +3,13 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
-  describe "GET 'home'" do
-    it "should be successful" do
-      get 'home'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'contact'" do
-    it "should be successful" do
-      get 'contact'
-      response.should be_success
+  %w(home contact about).each do |action|
+    describe "GET '#{action}'" do
+      it "should be successful" do
+        get action
+        response.should be_success
+      end
     end
   end
   
-  describe "GET 'about'" do
-    it "should be successful" do
-      get 'about'
-      response.should be_success
-    end
-  end
-
 end
