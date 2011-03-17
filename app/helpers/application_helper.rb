23656c36af0352e0e955c84e3d2c1efd6edb05e1
Aaron Sheriff <aaron.sheriff@gmail.com>
2011-03-17 13:41:21 -0500

    Added #title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def title
+    base_title = "My Silly Rials App"
+    if @title && !@title.empty?
+      "#{base_title} :: #{@title}"
+    else
+      base_title
+    end
+  end
 end
