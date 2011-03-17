3c15d2c26ecdc9fb3e5382d77bc2086843302297
Aaron Sheriff <aaron.sheriff@gmail.com>
2011-03-17 17:24:34 -0500

    Added global links, request spec

---
+++ spec/requests/global_links_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe "GlobalLinks" do
+  before(:each) do
+    @base_title = "My Silly Rials App"
+  end
+  
+  %w(contact about help).each do |action|
+    name = action.capitalize
+    url = "/#{action}"
+   
+    it "should have a #{name} page at #{url}" do
+      get url
+      response.should have_selector("title",
+        :content => "#{@base_title} :: #{name}"
+      )
+    end
+  end
+  
+  it "should have a Home page at /" do
+      get "/"
+      response.should have_selector("title",
+        :content => "#{@base_title} :: Home"
+      )
+  end
+end
