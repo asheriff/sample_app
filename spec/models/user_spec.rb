575f7db9172eae09813d610aa6eb0ddd5c4adac3
Aaron Sheriff <aaron.sheriff@gmail.com>
2011-03-18 16:30:52 -0500

    Added User validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,41 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @attrs = { :name=>"Jon Doe", :email=>"jdoe@foo.com" }
+  end
+  
+  it "should create new users given valid data" do
+    User.create! @attrs
+  end
+  
+  it "should require a name" do
+    user = User.new( @attrs.merge(:name=>"") )
+    user.valid?.should eq false
+  end
+  
+  it "should reject long names" do
+    name = "a"*51
+    user = User.new( @attrs.merge(:name=>name) )
+    user.valid?.should eq false
+  end
+  
+  it "should require a email" do
+    user = User.new( @attrs.merge(:email=>"") )
+    user.valid?.should eq false
+  end
+  
+  it "should reject invalid email addresses" do
+    emails = %w(foo@bar,com foo_bar.com foo.bar@baz.)
+    emails.each do |email|
+      user = User.new( @attrs.merge(:email=>email) )
+      user.valid?.should eq false
+    end
+  end
+  
+  it "should reject duplicate email addresses" do
+    User.create! @attrs
+    user2 = User.new @attrs.merge(:email=>@attrs[:email].upcase)
+    user2.valid?.should eq false
+  end
 end
