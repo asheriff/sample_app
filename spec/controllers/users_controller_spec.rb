fcb9c0f6bedfe8cad57c57377ffc439e52add115
Aaron Sheriff <aaron.sheriff@gmail.com>
2011-03-22 18:03:28 -0500

    Added User#update page

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -111,4 +111,34 @@ describe UsersController do
       end
     end
   end
+
+  describe "GET 'edit'" do
+    before :each do
+      @user = Factory(:user)
+      test_sign_in(@user)
+    end
+    
+    it "should be successful" do
+      get 'edit', :id => @user
+      response.should be_success
+    end
+    
+    it "should have the correct title" do
+      get 'edit', :id => @user
+      response.should have_selector("title",
+        :content => "#{@base_title} :: #{@user.name} :: Edit"
+      )
+    end
+    
+    it "should have a link to change gravatar" do
+      get 'edit', :id => @user
+      gravatar_url = "http://gravatar.com/emails"
+      response.should have_selector("a",
+        :href => gravatar_url,
+        :content => "Change"
+      )
+    end
+    
+  end
+
 end
