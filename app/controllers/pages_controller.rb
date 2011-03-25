4356340fb1e569c989e0a0af31b845a5d79a537a
Aaron Sheriff <aaron.sheriff@gmail.com>
2011-03-25 16:02:25 -0500

    Added feed to /home screen

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
   def home
-    @micropost = Micropost.new if signed_in?
+    if signed_in?
+      @micropost = Micropost.new if signed_in?
+      @feed_items = current_user.feed.paginate( :page=>params[:page], :per_page=>10 )
+    end
   end
 
   def contact
